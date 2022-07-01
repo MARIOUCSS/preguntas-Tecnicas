129769416531b16f5af934ada33867e5474bde2c
MARIOUCSS <jcvdbocanegra@gmail.com>
2022-06-30 23:11:07 -0500

    segunda ingreso

--- src/entrevistas/Entrevistas.java
+++ src/entrevistas/Entrevistas.java
@@ -36,25 +36,27 @@ public class Entrevistas {
 //        mostararray(c);
 
 //3.  Ingresar si de 3 en 3 en un nuevo array y insertalo en uno nuevo
-        int a[] = {1, 2, 3, 4, 5, 6};
-        int b[] = { 7, 8, 9, 10, 11,12};
-        int c[] = new int[12];
-        int j = 0;
-        int z=0;
-        while (z < 6) {
-            for (int i = 0; i < 3; i++) {
-                c[j] = a[i+z];
-                j++;
-            }
+//        int a[] = {1, 2, 3, 4, 5, 6};
+//        int b[] = { 7, 8, 9, 10, 11,12};
+//        int c[] = new int[12];
+//        int j = 0;
+//        int z=0;
+//        while (z < 6) {
+//            for (int i = 0; i < 3; i++) {
+//                c[j] = a[i+z];
+//                j++;
+//            }
+//
+//            for (int k = 0; k < 3; k++) {
+//                c[j] = b[k+z];
+//                 j++;
+//            }
+//            z = z + 3;
+//
+//        }
+//        mostararray(c);
 
-            for (int k = 0; k < 3; k++) {
-                c[j] = b[k+z];
-                 j++;
-            }
-            z = z + 3;
-
-        }
-        mostararray(c);
+//vamos a mostrar la pruebas siguientes para
     }
 
 }
