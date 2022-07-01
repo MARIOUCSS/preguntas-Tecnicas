9a09a8961e899190bb1ac50ca55a96eb56ec5edd
MARIOUCSS <jcvdbocanegra@gmail.com>
2022-06-30 22:53:39 -0500

    Se agrega el Proyecto

---
+++ src/entrevistas/Entrevistas.java
@@ -0,0 +1,60 @@
+package entrevistas;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Entrevistas {
+
+    static public void mostararray(int t[]) {
+        for (int i = 0; i < t.length; i++) {
+            System.out.println(t[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+//      1.-Mostrar en el orden sgte 123121;
+//
+//        for (int i = 0; i <3; i++) {
+//            int j=i+1;
+//            while (j<4) {                
+//                System.out.println(j-i);
+//                j++;
+//            }
+//        }
+//  2.-2 vectores  de longitud 5 unelos uno de cada en un nuevo array de 10
+//        int a[] = {1, 2, 3, 4, 5};
+//        int b[] = {6, 7, 8, 9, 10};
+//        int c[] = new int[10];
+//        int j = 0;
+//        for (int i = 0; i <5; i++) {
+//            c[j] = a[i];
+//            j++;
+//            c[j] = b[i];
+//            j++;
+//        }
+//        mostararray(c);
+
+//3.  Ingresar si de 3 en 3 en un nuevo array y insertalo en uno nuevo
+        int a[] = {1, 2, 3, 4, 5, 6};
+        int b[] = { 7, 8, 9, 10, 11,12};
+        int c[] = new int[12];
+        int j = 0;
+        int z=0;
+        while (z < 6) {
+            for (int i = 0; i < 3; i++) {
+                c[j] = a[i+z];
+                j++;
+            }
+
+            for (int k = 0; k < 3; k++) {
+                c[j] = b[k+z];
+                 j++;
+            }
+            z = z + 3;
+
+        }
+        mostararray(c);
+    }
+
+}
